4731d0d6666908669aa2c53883e7f97319279bf5
krystian.zakrys <krystian.zakrys@gmail.com>
2020-09-01 21:15:12 +0200

    Add hero details component.

---
+++ src/app/hero.ts
@@ -0,0 +1,9 @@
+export interface Hero{
+    id: number;
+    name: string;
+    avatarUrl: string;
+    type: string;
+    speciality: string;
+    skills: string[];
+    spell: string;
+}
---
+++ src/app/heroes-mock.ts
@@ -0,0 +1,7 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Christian', avatarUrl: 'https://heroes.thelazy.net/images/archive/f/f8/20180130004359%21Hero_Christian.png', type: 'Knight', skills: ['Basic Leadership','Basic Artilery'], speciality: 'Ballista', spell: ''},
+    {id: 2, name: 'Edric', avatarUrl: 'https://heroes.thelazy.net/images/archive/7/7f/20180130004426%21Hero_Edric.png', type: 'Knight', skills: ['Basic Leadership','Basic Armorer'], speciality: 'Griffins', spell: ''},
+    {id: 3, name: 'Orrin', avatarUrl: 'https://heroes.thelazy.net/images/4/4a/Hero_Orrin.png', type: 'Knight', skills: ['Basic Leadership','Basic Archery'], speciality: 'Archery', spell: ''},
+]
